5bea736f22c0cb16c388fbfab6e77260b2b0e5d2
Ulan8888 <tezjet888@gmail.com>
2023-10-11 01:16:08 +0000

    new

--- variables.tf
+++ variables.tf
@@ -57,8 +57,3 @@ variable instance_type {
     default = ""
 }
 
-variable vpc_cidr {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"  # Replace with your desired CIDR block
-}
